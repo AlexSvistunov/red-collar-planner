6e314e6d87076333764c0562edd313dc14e701a1
Alexey Svistunov <xdayx53@gmail.com>
2024-07-18 21:04:40 +0300

    change api url

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,2 @@
-// export const URL = 'https://planner.rdclr.ru'
-export const URL = 'http://localhost:1337'
+export const URL = 'https://planner.rdclr.ru'
+// export const URL = 'http://localhost:1337'
